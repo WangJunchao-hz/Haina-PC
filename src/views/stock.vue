77044527f6cb610ef0673f0f7f9e58e66e741fdc
wangl <wanglujiu@foxmail.com>
2024-01-15 10:15:06 +0800

    🐛fix:
    提交人：王亮

--- src/views/stock.vue
+++ src/views/stock.vue
@@ -125,7 +125,7 @@ import { GetRobotData } from '@/common/api/ths-wen-cai-api'
 import { replaceTpl, resolutionStock } from '@/common/utils'
 import dayjs from 'dayjs'
 const q =
-	'昨日涨停最终涨停时间从早到晚排序，非st，非北交所，昨日涨停最大封单量，概念，昨日几天几板，昨日连续涨停天数，今日竞价涨跌幅'
+	'昨日涨停最终涨停时间从早到晚排序，非st，非北交所，非停牌，昨日涨停最大封单量，概念，昨日几天几板，昨日连续涨停天数，今日竞价涨跌幅'
 const date = ref<string>(dayjs().format('YYYY-MM-DD'))
 const lists = ref<{
 	lbs: any[]
